cf9a523941dc1eb990f316988756571f22d32cc6
Taxenmaxen <39467732+Taxenmaxen@users.noreply.github.com>
2024-01-15 09:39:42 +0100

    ÖvningPages

---
+++ ÖvningPages/jsHandler.js
@@ -0,0 +1,18 @@
+function doCalc(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let operator = document.getElementById("operator").value;
+
+    if(operator == "volvo"){
+        document.getElementById("res").innerHTML = "Add) --> " + num1 + " + " + num2 + " = " + (+num1 + +num2);
+    }
+    else if(operator == "saab"){
+        document.getElementById("res").innerHTML = "Sub) --> " + num1 + " - " + num2 + " = " + (+num1 - +num2);
+    }
+    else if(operator == "mercedes"){
+        document.getElementById("res").innerHTML = "Mul) --> " + num1 + " * " + num2 + " = " + (+num1 * +num2);
+    }
+    else{
+        document.getElementById("res").innerHTML = "Div) --> " + num1 + " / " + num2 + " = " + (+num1 / +num2);
+    }
+}
