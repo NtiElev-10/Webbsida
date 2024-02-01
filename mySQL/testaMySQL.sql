2bc0dd71df591550099d77220dd0e2a45fcd9a51
Taxenmaxen <39467732+Taxenmaxen@users.noreply.github.com>
2024-02-01 16:34:46 +0100

    Update testaMySQL.sql

--- mySQL/testaMySQL.sql
+++ mySQL/testaMySQL.sql
@@ -3,13 +3,16 @@ Create database if not exists myTestDB;
 Use myTestDB;
 
 create table if not exists user(
-ID int,
-username varchar(100),
-email varchar(100),
-password varchar(100)
+ID int primary key,
+username varchar(100) NOT NULL,
+email varchar(100) NOT NULL,
+password varchar(100) NOT NULL
 );
 
 Insert into user(ID, username, email, password) values(1, "mitt användarnamn", "användarnamn@gmail.com", "hejhej123");
+
+alter table user 
+add exempel bool;
 
 select * from user; 
 
