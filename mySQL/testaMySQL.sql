ccdbab35260bf4f941367c171a789ffce693b45b
Taxenmaxen <39467732+Taxenmaxen@users.noreply.github.com>
2024-02-07 14:30:59 +0100

    hej Max

--- mySQL/testaMySQL.sql
+++ mySQL/testaMySQL.sql
@@ -9,11 +9,17 @@ email varchar(100) NOT NULL,
 password varchar(100) NOT NULL
 );
 
+create table if not exists user2(
+username varchar(100) NOT NULL,
+email varchar(100) NOT NULL
+);
+
+
 Insert into user(ID, username, email, password) values(1, "mitt användarnamn", "användarnamn@gmail.com", "hejhej123");
 
-alter table user 
-add exempel bool;
+insert into user2 select username, email from user;
 
-select * from user; 
+select * from user2; 
+
 
 drop database myTestDB;
